08f4012bf053ae1e3f78b1c1e981e1bfe11f60a2
Martin Šutka <martin.sutka@slovanet.net>
2023-04-07 20:09:15 +0200

    Load toc

--- js/blog.js
+++ js/blog.js
@@ -3,6 +3,7 @@
 
     var doc = global.document;
     var mdc = global.mdc;
+    var toc = [];
 
     //#endregion
 
@@ -23,6 +24,34 @@
         }
     };
 
+
+    /**
+     * Loads toc content.
+     */
+    function loadToc() {
+        return fetch("https://api.github.com/repos/xxxmatko/blog/contents/docs/toc.json", {
+            method: "GET",
+            cache: "no-cache",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/vnd.github.raw",
+                "X-GitHub-Api-Version": "2022-11-28"
+            }
+        }).then(function(response) {
+            if(response.ok) {
+                return response.json();
+            }
+
+            return null;
+        }).then(function (response) {
+            if(!response) {
+                return;
+            }
+
+            toc = response.value;
+        });
+    };
+
     //#endregion
 
 
@@ -33,6 +62,12 @@
      */
     ready(function () {
         mdc.autoInit();
+
+        loadToc().then(function() {
+            toc.forEach(function(article) {
+                console.warn(article);
+            });
+        });        
     });
 
     //#endregion    
